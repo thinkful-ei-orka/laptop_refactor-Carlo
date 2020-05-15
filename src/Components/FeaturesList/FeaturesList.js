63b657d6fdc9f7cb3247bb59356a6d0e3db21d84
Carlo Paredes <c.nick.paredes@gmail.com>
2020-05-15 08:20:58 -0700

    Fixed key on FeatureList

--- src/Components/FeaturesList/FeaturesList.js
+++ src/Components/FeaturesList/FeaturesList.js
@@ -8,7 +8,7 @@ class FeaturesList extends React.Component {
         const features = Object.keys(this.props.features).map((feature, idx) => {
             const featureHash = feature + '-' + idx;
 
-            return <Feature featureHash={featureHash} currentFeature={feature} features={this.props.features} updateFeature={this.props.updateFeature} selected={this.props.selected} />
+            return <Feature key={featureHash} currentFeature={feature} features={this.props.features} updateFeature={this.props.updateFeature} selected={this.props.selected} />
           });
 
 
